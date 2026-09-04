f83d99416ea05b2694d5a6add7bfd78d4dd9af26
ProjectJ <44721506+thdsladk@users.noreply.github.com>
2026-09-04 12:52:07 +0900

    feat: 스포너의 적 스폰 주기 스폰 할 때마다 랜덤

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    // Header는 에디터 화면에서 이름을 달아주기 위한 기능.
     // 필요 속성
-    // - xkdlaj
+    // - 타이머
     [Header("스폰 간격")] [SerializeField] private float _spawnInterval = 3f;
     private float _timer;
 
@@ -17,6 +18,8 @@ public class EnemySpawner : MonoBehaviour
         if (_timer >= _spawnInterval)
         {
             _timer = 0;
+            _spawnInterval = UnityEngine.Random.Range(1f, 3f);  // float : 1f ~ 3f
+            int randomInt = Random.Range(1, 3);                 // int : 1 ~ 2
             Spawn();
         }
     }
