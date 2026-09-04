6c3fb3dc225b79656351d705627ede2331432362
ProjectJ <44721506+thdsladk@users.noreply.github.com>
2026-09-04 11:01:19 +0900

    feat: 테스트 코드

---
+++ Assets/02.Scripts/GeminiReviewParticle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GeminiReviewParticle : MonoBehaviour
+{
+     private void Start()
+    {
+        
+    }
+
+     private void Update()
+    {
+        
+        Debug.Log($"Player Position: {transform.position}");
+    }
+}
