627b515418978168519025994a30281b666fe861
ProjectJ <44721506+thdsladk@users.noreply.github.com>
2026-09-07 15:37:01 +0900

    feat: 좌우이동 애니메이션 추가

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviour
     // 목적: 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
 
     // 필요 필드:
+    private Animator _animator;
     [SerializeField] private float _speed;
     [SerializeField] private float _maxSpeed;
 
@@ -16,6 +17,11 @@ public class PlayerMove : MonoBehaviour
     public float MaxPositionX;
     public float MinPositionX;
 
+    private void Awake()
+    {
+        // 애니메이터 컴포넌트에 대한 참조를 가져와서 할당한다.
+        _animator = GetComponent<Animator>();
+    }
 
     // 매 프레임마다 실행된다.
     // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이
@@ -56,9 +62,11 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
+
         // 2. 키보드 입력에 따라 방향을 구한다.
         Vector2 normalizedDirection = new Vector2(h, v).normalized;
 
+        _animator.SetInteger("X", (int)normalizedDirection.x);
         // 3. 방향과 속력에 따라 이동한다.
         Vector2 newPosition = transform.position + (Vector3)normalizedDirection * _speed * Time.deltaTime;
 
