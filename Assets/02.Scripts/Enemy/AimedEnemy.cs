03bc850483f91067b17799367491590a127cf547
ProjectJ <44721506+thdsladk@users.noreply.github.com>
2026-09-08 11:15:34 +0900

    docs: Atan를 이용한 회전 주석 추가

--- Assets/02.Scripts/Enemy/AimedEnemy.cs
+++ Assets/02.Scripts/Enemy/AimedEnemy.cs
@@ -14,7 +14,17 @@ public class AimedEnemy : Enemy
         }
 
         _direction = (_player.transform.position - transform.position).normalized;
-
+        /* atan로 각도 구하기
+         *  float dx = _direction.x;
+         *  float dx = _direction.x;
+         *  tan0 = dy/ dx
+         *  tan^ * tan0 = tan^ * dy/ dx
+         *  float seta = Mathf.Atan2(dy,dx);
+         *  float angle = seta * Mathf.Rad2Deg + 90f;           // 원본 이미지가 아래를 보고 있어서 90도 추가
+         *  transform.rotation = Quaternion.Euler(0f,0f,angle);
+         *  _direction.Normalize();
+         *  transfoorm.position += (Vector3)(_direction * _moveSpeed) * Time.deltaTime;
+         */
         //  바라보게 하기 
         transform.up = -_direction;
     }
