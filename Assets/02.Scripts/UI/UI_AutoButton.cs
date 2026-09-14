365bcf3df2cb1fd2e3532bb45d8e035dbefcff04
ProjectJ <44721506+thdsladk@users.noreply.github.com>
2026-09-14 11:19:49 +0900

    refactor: 버튼 클릭 기능 분리
    버튼 클릭을 UI_ButtonClick으로 분리

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -13,46 +13,21 @@ public class UI_AutoButton : MonoBehaviour
 
     [SerializeField] private Sprite _offSprite;
 
-    [Header("클릭시 애니메이션")]
-    [SerializeField] private AnimationCurve _bumpCurve;
-
-    private AudioSource _audioSource;
     private bool _autoMode = false;
     private Player _player;
-
-    private float _scale = 1.0f;
-    private bool _isBumping = false;
-    private float _elapsedTime = 0.0f;
-    private const float _bumpDuration = 0.6f;
-    private const float _bumpScale = 1.0f;
 
 
     private void Start()
     {
         _player = GameObject.FindAnyObjectByType<Player>();
         _myImage = GetComponent<Image>();
-        _audioSource = GetComponent<AudioSource>();
+
 
         AutoToggle();
     }
 
     private void Update()
     {
-        if (!_isBumping) return;
-
-        // 1. 경과 시간 누적 
-        _elapsedTime += Time.deltaTime;
-        if (_elapsedTime > _bumpDuration)
-        {
-            transform.localScale = Vector3.one;
-            _isBumping = false;
-            return;
-        }
-
-        // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
-        float time = _elapsedTime / _bumpDuration; // 얼마나 지났는지 퍼센트 ( 0 ~ 1 )
-        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
-        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * _bumpScale, curveValue);
     }
 
     public void AutoToggle()
@@ -65,17 +40,8 @@ public class UI_AutoButton : MonoBehaviour
         _myImage.sprite = (_autoMode) ? _onSprite : _offSprite;
     }
     // todo : 버튼 클릭할 때 애니메이션 주기 + 사운드 주기 
+    // 애니메이션 컴포넌트 : UI_ButtonClick
+
     // 애니메이션 : 코드로 구현 약간 커졌다가 작아지기...
     // 사운드 : 일레븐랩스에서 버튼 클릭 공용 사운드 만들어서 적용
-
-    public void PlaySound()
-    {
-        _audioSource.Play();
-    }
-
-    public void PlayButtonAnimation()
-    {
-        _isBumping = true;
-        _elapsedTime = 0.0f;
-    }
 }
---
+++ Assets/02.Scripts/UI/UI_ButtonClick.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ButtonClick : MonoBehaviour
+{
+    private Button _button;
+    private AudioSource _audioSource;
+
+    [Header("클릭시 애니메이션")]
+    [SerializeField] private AnimationCurve _bumpCurve;
+
+
+    private float _scale = 1.0f;
+    private bool _isBumping = false;
+    private float _elapsedTime = 0.0f;
+    private const float BumpDuration = 0.6f;
+    private const float BumpScale = 1.0f;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(PlayButtonAnimation);
+        _button.onClick.AddListener(PlaySound);
+    }
+
+    private void Update()
+    {
+        if (!_isBumping) return;
+
+        // 1. 경과 시간 누적 
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime > BumpDuration)
+        {
+            transform.localScale = Vector3.one;
+            _isBumping = false;
+            return;
+        }
+
+        // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
+        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 ( 0 ~ 1 )
+        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
+        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * BumpScale, curveValue);
+    }
+
+    public void PlaySound()
+    {
+        _audioSource.Play();
+    }
+
+    public void PlayButtonAnimation()
+    {
+        _isBumping = true;
+        _elapsedTime = 0.0f;
+    }
+}
